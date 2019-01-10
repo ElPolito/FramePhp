39b3dd3cdbb44b2b9301961c6d16a3d52dd6b4a0
Polo <pablo_mo@orange.fr>
2019-01-10 19:49:09 +0100

    Debug

--- _config/connectdb.php
+++ _config/connectdb.php
@@ -1,17 +1,21 @@
 <?php 
 	
 	// Define your database informations
-	define("HOST", "localhost");
-	define("USER", "root");
+	define("HOST", "");
+	define("USER", "");
 	define("PASS", "");
-	define("DB", "mydb");
+	define("DB", "");
 
-	try{
-		$db = new PDO('mysql:dbname=' . DB . ';host=' . HOST, USER, PASS);
-	}catch(PDOException $e){
-		echo $e;
-		die();
+	if(HOST != "") {
+		try{
+			$db = new PDO('mysql:dbname=' . DB . ';host=' . HOST, USER, PASS);
+		}catch(PDOException $e){
+			echo $e;
+			die();
+		}	
 	}
+
+	
 
 	// If you need you can define other databases
 

f9b6a2d4a6d14351d6db7e16ee2eaa562180ebf3
Polo <pablo_mo@orange.fr>
2019-01-26 10:07:47 +0100

    Changement des services en classe

--- Services/error.php
+++ Services/error.php
@@ -1,11 +1,20 @@
 <?php 
 	
-	// This service allow you to redirect your visitors to a 404 error page
-	// You can custom this page in Templates/_Errors/404.php 
-	function redirectToError () {
-		http_response_code(404);
-		require("Templates/_Errors/404.php");
-		die();
+	namespace Project\Services;
+
+	use Project\Controllers;
+
+	class Redirect {
+
+		// This service allow you to redirect your visitors to a 404 error page
+		// You can custom this page in Templates/_Errors/404.php 
+		public function redirectToError () {
+			http_response_code(404);
+			$controller = new ErrorController ();
+			echo $controller->error404();
+			die();
+		}	
+
 	}
 
 ?>
